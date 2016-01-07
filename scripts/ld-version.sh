a431d2c753578f201a3a886189ccaa6fd78d3783
Alexandre Oliva <lxoliva@fsfla.org>
2016-01-07 16:49:35 -0200

    Fix ld-version.sh to work with mawk and gnewsense 3 linker
    $ ld --version
    GNU ld (GNU Binutils for Debian) 2.20.1-system.20100303
    
    The date and the -system confused the version script, and mawk did not
    take the closing parentheses as a literal, but as a grouping character.
    
    Signed-off-by: Alexandre Oliva <lxoliva@fsfla.org>

--- scripts/ld-version.sh
+++ scripts/ld-version.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/awk -f
 # extract linker version number from stdin and turn into single number
 	{
-	gsub(".*)", "");
-	split($1,a, ".");
-	print a[1]*100000000 + a[2]*1000000 + a[3]*10000 + a[4]*100 + a[5];
+	gsub(".*[)]", "");
+	split($1,a, "[-.]");
+	printf "%i%04i\n", a[1]*10000 + a[2]*100 + a[3], (a[4]*100 + a[5])%10000;
 	exit
 	}
