44251da9262f57e94ba2bbdbbea9d0ff1ce0e507
Huaqi Fang <578567190@qq.com>
2021-02-26 11:30:09 +0800

    tty: serial: add driver for Nuclei UART/USART
    Add new Nuclei UART/USART driver for Nuclei Evaluation SoC
    on FPGA evaluation board.
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>
    
    drivers: add support for nuclei uart SETUP reg
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>
    
    serial:nuclei: report max baud rate when boot
    
    see https://github.com/torvalds/linux/commit/0a7ff843d507ce2cca2c3b7e169ee56e28133530
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -231,6 +231,9 @@
 /* Sunplus UART */
 #define PORT_SUNPLUS	123
 
+/* Nuclei UART */
+#define PORT_NUCLEI	124
+
 /* Generic type identifier for ports which type is not important to userspace. */
 #define PORT_GENERIC	(-1)
 
