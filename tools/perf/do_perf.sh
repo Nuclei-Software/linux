50a9e8495fa56e1188c47d16ddc10babe5f09e43
Huaqi Fang <578567190@qq.com>
2024-08-16 10:12:56 +0800

    introduce two script to build perf
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ tools/perf/do_perf.sh
@@ -0,0 +1,44 @@
+#!/bin/env bash
+
+if [ ! $# -eq 4 ] ; then
+    echo "Help: $0 <cross_compile> <arch> <abi> <install>"
+    echo "eg. $0 riscv64-unknown-linux-gnu rv64imafdc lp64d install"
+    exit 1
+fi
+
+CROSS_COMPILE=$1
+ARCH=$2
+ABI=$3
+INSTDIR=$4
+
+SCRIPTDIR=$(dirname $(readlink -f $BASH_SOURCE))
+SCRIPTDIR=$(readlink -f $SCRIPTDIR)
+
+ARCHABI_FLAGS="-march=$ARCH -mabi=$ABI"
+
+function build_perf {
+    local srcdir=$1
+    local instdir=$2
+    echo "Build Linux Tool Perf"
+    pushd $srcdir
+    local srctree=$(readlink -f $(pwd)/../../)
+    make srctree=$srctree ARCH=riscv CROSS_COMPILE=${CROSS_COMPILE}- LDFLAGS="${ARCHABI_FLAGS} -L${instdir}/lib" EXTRA_CFLAGS="${ARCHABI_FLAGS} -I${instdir}/include" DESTDIR=$instdir clean
+    # need extra ldflags -lz to check libelf
+    make srctree=$srctree ARCH=riscv CROSS_COMPILE=${CROSS_COMPILE}- LDFLAGS="${ARCHABI_FLAGS} -L${instdir}/lib -lz" EXTRA_CFLAGS="${ARCHABI_FLAGS} -I${instdir}/include" DESTDIR=$instdir -j install
+    popd
+}
+
+pushd $SCRIPTDIR
+
+if [ ! -d ${INSTDIR} ] ; then
+    echo "${INSTDIR} not exist! create it now!"
+    mkdir -p $INSTDIR
+fi
+
+INSTDIR=$(readlink -f $INSTDIR)
+
+build_perf $(pwd) $INSTDIR
+
+popd
+
+exit 0
---
+++ tools/perf/pre_perf.sh
@@ -0,0 +1,80 @@
+#!/bin/env bash
+
+if [ ! $# -eq 4 ] ; then
+    echo "Help: $0 <cross_compile> <arch> <abi> <install>"
+    echo "eg. $0 riscv64-unknown-linux-gnu rv64imafdc lp64d install"
+    exit 1
+fi
+
+CROSS_COMPILE=$1
+ARCH=$2
+ABI=$3
+LIB_INSTDIR=$4
+
+
+SCRIPTDIR=$(dirname $(readlink -f $BASH_SOURCE))
+SCRIPTDIR=$(readlink -f $SCRIPTDIR)
+
+ZLIB_SRCDIR=$(readlink -f $SCRIPTDIR/../../zlib)
+ELFUTILS_SRCDIR=$(readlink -f $SCRIPTDIR/../../elfutils)
+LIBTRACEEVENT_SRCDIR=$(readlink -f $SCRIPTDIR/../../libtraceevent)
+
+ARCHABI_FLAGS="-march=$ARCH -mabi=$ABI"
+
+function build_zlib {
+    local srcdir=$1
+    local instdir=$2
+    echo "Build ZLib"
+    pushd ${srcdir}
+    rm -rf install
+    make ARCH_ABI="${ARCHABI_FLAGS}" clean
+    make ARCH_ABI="${ARCHABI_FLAGS}" -j install
+    echo "Copy built zlib to ${instdir}"
+    cp -rf install/* ${instdir}/
+    popd
+}
+
+function build_elfutils {
+    local srcdir=$1
+    local instdir=$2
+    echo "Build elfutils"
+    pushd $srcdir
+    autoreconf -i -f
+    rm -rf install
+    CFLAGS="${ARCHABI_FLAGS}" CXXFLAGS="${ARCHABI_FLAGS}" LDFLAGS="${ARCHABI_FLAGS} -L${instdir}/lib" CPPFLAGS="-I${instdir}/include" ./configure --host=${CROSS_COMPILE} --prefix=$(pwd)/install --disable-libdebuginfod --enable-maintainer-mode  --disable-debuginfod
+    make clean
+    make -j install
+    cp -rf install/* ${instdir}/
+    popd
+}
+
+function build_libtraceevent {
+    local srcdir=$1
+    local instdir=$2
+    echo "Build libtraceevent"
+    pushd $srcdir
+    rm -rf install
+    make CROSS_COMPILE=${CROSS_COMPILE}- CONFIG_FLAGS="${ARCHABI_FLAGS}" LDFLAGS="${ARCHABI_FLAGS}" DESTDIR=$(pwd)/install clean
+    make CROSS_COMPILE=${CROSS_COMPILE}- CONFIG_FLAGS="${ARCHABI_FLAGS}" LDFLAGS="${ARCHABI_FLAGS}" DESTDIR=$(pwd)/install libdir_relative=lib prefix= pkgconfig_dir=/lib/pkgconfig -j install
+    cp -rf install/* ${instdir}/
+    popd
+}
+
+pushd $SCRIPTDIR
+
+if [ ! -d ${LIB_INSTDIR} ] ; then
+    echo "${LIB_INSTDIR} not exist! create it now!"
+    mkdir -p $LIB_INSTDIR
+fi
+
+LIB_INSTDIR=$(readlink -f $LIB_INSTDIR)
+
+build_zlib $ZLIB_SRCDIR $LIB_INSTDIR
+build_elfutils $ELFUTILS_SRCDIR $LIB_INSTDIR
+build_libtraceevent $LIBTRACEEVENT_SRCDIR $LIB_INSTDIR
+
+popd
+
+echo "Please find prebuilt zlib, libelf and libtraceevent in $LIB_INSTDIR"
+
+exit 0
